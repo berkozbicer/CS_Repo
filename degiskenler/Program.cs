a2621ab232db4ff31d6128ccf724311fb8bd387e
Berk OZBICER <zer0zer02wo@gmail.com>
2025-08-28 15:27:38 +0300

    Initial commit - Add C# learning projects and README

---
+++ degiskenler/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace degisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 5;             // 1 byte
+            sbyte sb = 5;           // 1 byte   
+            short s = 5;            // 2 byte
+            ushort us = 5;          // 2 byte
+            Int16 i16 = 2;          // 2 byte
+            int i = 2;              // 4 byte
+            Int32 i32 = 2;          // 4 byte
+            Int64 i64 = 2;          // 8 byte
+            uint ui = 2;            // 4 byte
+            long l = 4;             // 8 byte
+            ulong ul = 4;           // 8 byte
+            float f = 5;            // 4 byte
+            double d = 5;           // 8 byte
+            decimal de = 5;         // 16 byte
+            char c = '2';          // 2 byte
+            string str = "Patika";     // sınırsız
+            bool b1 = true;         // 1 byte
+            DateTime dt = DateTime.Now; // 8 byte
+            Console.WriteLine(dt);
+            object o1 = "x";        // sınırsız
+            object o2 = 'y';        // 1 byte
+            object o3 = 4;          // 4 byte
+            object o4 = 4.3;        // 8 byte
+            // string ifadeler
+            string str1 = string.Empty; // boş string
+            str1 = "Patika";
+            string ad = "Ayşe";
+            string soyad = "Yılmaz";
+            string tamIsim = ad + " " + soyad;
+            // integer tanımlama şekilleri
+            int integer1 = 5;
+            int integer2 = 3;
+            int integer3 = integer1 * integer2;
+            // boolean
+            bool bool1 = 10 < 2;
+            // değişken dönüşümleri
+            string str20 = "20";
+            int int20 = 20;
+            string yeniDeger = str20 + int20.ToString(); // "2020"
+            Console.WriteLine(yeniDeger); // 2020   
+            int int21 = int20 + Convert.ToInt32(str20); // 40
+            Console.WriteLine(int21); // 40 
+            int int22 = int20 + int.Parse(str20); // 40
+            Console.WriteLine(int22); // 40
+            // datetime
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");  // 27.06.2024
+            Console.WriteLine(datetime);
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy"); // 27/06/2024
+            Console.WriteLine(datetime2);
+            string hour = DateTime.Now.ToString("HH:mm"); // 14:35  
+            Console.WriteLine(hour);
+            // saat dakika
+            string minute = DateTime.Now.ToString("mm"); // 35
+            Console.WriteLine(minute);
+            string second = DateTime.Now.ToString("ss"); // 45
+            Console.WriteLine(second);
+        }
+    }
+}
