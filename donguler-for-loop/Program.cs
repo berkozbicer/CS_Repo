a728529e5576d24ff2ee483d2efcc703833bc9c2
Berk OZBICER <zer0zer02wo@gmail.com>
2025-09-04 15:14:13 +0300

    Fix embedded repos: add as normal folders

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz:");
+            int girilenSayi = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= girilenSayi; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("İşlem tamamlandı.");
+
+            // 1 ile 1000 arasındaki tek ve çift sayıların kendi içlerindeki toplamlarını ekrana yazdır.
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                {
+                    ciftToplam += i;
+                }
+            }
+            Console.WriteLine("Tek sayıların toplamı: " + tekToplam);
+            Console.WriteLine("Çift sayıların toplamı: " + ciftToplam);
+
+            // break, continue
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("İşlem tamamlandı."); 
+        }
+    }
+}
---
+++ if-else-if/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Good Morning!");
+            else if (time <= 18)
+                Console.WriteLine("Good Afternoon!");
+            else
+                Console.WriteLine("Good Night!");
+
+            string sonuc = time <= 18 ? "Good Afternoon!" : "Good Night!";
+            
+            sonuc = time >= 6 && time < 11 ? "Good Morning!" : time <= 18 ? "Good Afternoon!" : "Good Night!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
